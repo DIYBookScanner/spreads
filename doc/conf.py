88c81c263f2bb785afe807b435d0637dc66dabf3
Johannes Baiter <johannes.baiter@gmail.com>
2013-07-01 15:16:34 +0200

    Fix autodoc errors for ReadTheDocs

--- doc/conf.py
+++ doc/conf.py
@@ -13,6 +13,12 @@
 
 import os
 import sys
+
+import mock
+MOCK_MODULES = ['usb', 'stevedore.extension', 'stevedore.named']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 sys.path.append(os.path.abspath('_themes'))
 sys.path.insert(0, os.path.abspath(os.path.join('..')))
 
