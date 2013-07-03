68da3552d58c421a7b5515fb4610b9df3b56f57e
Johannes Baiter <johannes.baiter@gmail.com>
2013-07-04 00:05:13 +0200

    Implement IntroPage, ConnectPage and ConfigurePage

--- spreadsplug/gui/gui.py
+++ spreadsplug/gui/gui.py
@@ -1,7 +1,11 @@
+import time
+
+from concurrent.futures import ThreadPoolExecutor 
 from PySide import QtCore, QtGui
 
 import spreads.commands as cmd
 import spreads.util as util
+from spreads.plugin import get_devices
 
 import gui_rc
 
@@ -9,9 +13,9 @@ import gui_rc
 class SpreadsWizard(QtGui.QWizard):
     def __init__(self, config, parent=None):
         super(SpreadsWizard, self).__init__(parent)
-
         self.addPage(IntroPage(config))
         self.addPage(ConnectPage(config))
+        self.addPage(ConfigurePage(config))
         self.addPage(PreviewPage(config))
         self.addPage(CapturePage(config))
         self.addPage(PostprocessPage(config))
@@ -64,6 +68,16 @@ class IntroPage(QtGui.QWizardPage):
         dialog.exec_()
         self.line_edit.setText(dialog.selectedFiles()[0])
 
+    def validatePage(self):
+        self.wizard().project_path = self.line_edit.text()
+        if not self.wizard().project_path:
+            msg_box = QtGui.QMessageBox()
+            msg_box.setText("Please select a project directory.")
+            msg_box.setIcon(QtGui.QMessageBox.Critical)
+            msg_box.exec_()
+            return False
+        return True
+
 
 class ConnectPage(QtGui.QWizardPage):
     def __init__(self, config, parent=None):
@@ -74,12 +88,15 @@ class ConnectPage(QtGui.QWizardPage):
 
         self.setTitle("Connect")
 
+    def initializePage(self):
+        self.progress = QtGui.QProgressDialog("Detecting devices...",
+                                              "Cancel", 0, 0, self)
+
         self.label = QtGui.QLabel("Detecting devices...")
         self.devicewidget = QtGui.QListWidget()
         self.devicewidget.setSelectionMode(QtGui.QAbstractItemView
                                            .MultiSelection)
 
-        self.update_devices()
         refresh_btn = QtGui.QPushButton("Refresh")
         refresh_btn.clicked.connect(self.update_devices)
 
@@ -89,29 +106,70 @@ class ConnectPage(QtGui.QWizardPage):
         layout.addWidget(refresh_btn)
         self.setLayout(layout)
 
+        self.update_devices()
+
     def update_devices(self):
-        # TODO: Use some kind of activity indicator to keep users from
-        #       becoming impatient
         self.devicewidget.clear()
         self.label.setText("Detecting devices...")
-        devices = ["Camera A", "Camera B"]
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            future = executor.submit(get_devices)
+            while not future.done():
+                QtGui.qApp.processEvents()
+                self.progress.setValue(1)
+                time.sleep(0.001)
+            try:
+                devices = future.result()
+            except util.DeviceException:
+                devices = []
+        self.progress.close()
         if not devices:
             self.label.setText("<font color=red>No devices found!</font>")
         else:
             self.label.setText("Please select one or more devices:")
-        for device in devices:
+        self.devices = []
+        for idx, device in enumerate(devices, 1000):
             self.devicewidget.setCurrentItem(
-                QtGui.QListWidgetItem(device,
-                                      self.devicewidget))
+                QtGui.QListWidgetItem(device.__class__.__name__,
+                                      self.devicewidget, type=idx))
+            self.devices.append(device)
+
+    def nextId(self):
+        self.wizard().selected_devices = [self.devices[x.type()-1000]
+                                          for x
+                                          in self.devicewidget.selectedItems()]
+        if any([not x.orientation for x in self.wizard().selected_devices]):
+            return super(ConnectPage, self).nextId()
+        return super(ConnectPage, self).nextId()+1
+
+    def validatePage(self):
+        if not self.wizard().selected_devices:
+            msg_box = QtGui.QMessageBox()
+            msg_box.setText("No device selected.")
+            msg_box.setIcon(QtGui.QMessageBox.Critical)
+            msg_box.exec_()
+            return False
+        return True
 
 
 class ConfigurePage(QtGui.QWizardPage):
-    # TODO: This should only be viewed if the devices have not yet been
-    #       configured
-    def __init__(self, config, parent=None):
-        super(ConfigurePage, self).__init__(parent)
-
+    def initializePage(self):
         self.setTitle("Configure devices")
+        devices = [x for x in self.wizard().selected_devices
+                   if not x.orientation]
+        if len(devices) > 1:
+            orientation = "left"
+        else:
+            orientation = "right"
+        label = QtGui.QLabel("Please connect the {0} device."
+                             .format(orientation))
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(label)
+        self.setLayout(layout)
+
+    def nextId(self):
+        if any([not x.orientation for x in self.wizard().selected_devices]):
+            return super(ConnectPage, self).nextId()-1
+        return super(ConnectPage, self).nextId()
 
 
 class PreviewPage(QtGui.QWizardPage):
@@ -137,27 +195,37 @@ class CapturePage(QtGui.QWizardPage):
 
         self.setTitle("Capturing from devices")
 
-
-class DownloadPage(QtGui.QWizardPage):
-    def __init__(self, config, parent=None):
-        super(DownloadPage, self).__init__(parent)
-        # TODO: This shouldn't be a wizard page, but merely a progress
-        #       dialog!
-        # TODO: Change DevicePlugin API to make download_all_files an
-        #       iterator with __len__, so we can display a nice progress bar.
-
-        self.setTitle("Download")
+    def validatePage(self):
+        def download_images():
+            time.sleep(2)
+        #TODO: Display a real progress bar here, update it according to the
+        #      combined number of files in raw or its subfolders
+        progress = QtGui.QProgressDialog("Downloading files...",
+                                         "Cancel", 0, 0, self)
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            future = executor.submit(download_images)
+            while not future.done():
+                QtGui.qApp.processEvents()
+                progress.setValue(1)
+                time.sleep(0.001)
+        progress.close()
+        return True
 
 
 class PostprocessPage(QtGui.QWizardPage):
     def __init__(self, config, parent=None):
         super(PostprocessPage, self).__init__(parent)
         self.setTitle("Postprocessing")
-
+        # TODO: Make postprocess() a generator and update progress according
+        #       to it.
         # TODO: Update logbox from logging.info
+        self.progressbar = QtGui.QProgressBar(self)
+        self.progressbar.setRange(0, 0)
+        self.progressbar.setAlignment(QtCore.Qt.AlignCenter)
         self.logbox = QtGui.QTextEdit()
 
         layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.progressbar)
         layout.addWidget(self.logbox)
         self.setLayout(layout)
 
@@ -165,6 +233,6 @@ class PostprocessPage(QtGui.QWizardPage):
 class FinishPage(QtGui.QWizardPage):
     def __init__(self, config, parent=None):
         super(FinishPage, self).__init__(parent)
-
         self.setFinalPage(True)
         self.setTitle("Done!")
+        # TODO: Offer option to view project folder on exit
