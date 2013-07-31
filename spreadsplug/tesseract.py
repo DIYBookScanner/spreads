d0a21a643b2a6315b93e9897ed47d3d43557b95a
Johannes Baiter <johannes.baiter@gmail.com>
2013-07-31 14:02:35 +0200

    tesseract: Combine hOCR files into a single file during output stage

--- spreadsplug/tesseract.py
+++ spreadsplug/tesseract.py
@@ -1,8 +1,9 @@
 import logging
 import os
 import re
-
 import subprocess
+import xml.etree.cElementTree as ET
+
 from concurrent import futures
 
 from spreads.plugin import HookPlugin
@@ -50,3 +51,33 @@ class TesseractPlugin(HookPlugin):
                                      r'\g<1>', fp.read())
             with open(fname, 'w') as fp:
                 fp.write(new_content)
+
+    def output(self, path):
+        outfile = os.path.join(path, 'out', "{0}.hocr"
+                                            .format(os.path.basename(path)))
+        path = os.path.join(path, 'done')
+        out_root = ET.Element('html')
+        ET.SubElement(out_root, 'head')
+        body = ET.SubElement(out_root, 'body')
+
+        for idx, page in enumerate(sorted([os.path.join(path, x)
+                                   for x in os.listdir(path)
+                                   if x.endswith('.html')])):
+            # NOTE: Fixe some things from hOCR output so we can parse it...
+            with open(page, 'r+') as fp:
+                content = re.sub(r'<em><\/em>', '', fp.read())
+                content = re.sub(r'<strong><\/strong>', '', content)
+            page = ET.fromstring(content)
+            page_elem = page.find("xhtml:body/xhtml:div[@class='ocr_page']",
+                                  dict(xhtml="http://www.w3.org/1999/xhtml"))
+            # Correct page_number
+            page_elem.set('id', 'page_{0}'.format(idx))
+
+            # And tack it onto the output file
+            if page_elem:
+                body.append(page_elem)
+        with open(outfile, 'w') as fp:
+            # Strip those annoying namespace tags...
+            out_string = re.sub(r"(<\/*)html:", "\g<1>",
+                                ET.tostring(out_root))
+            fp.write(out_string.encode("UTF-8"))
