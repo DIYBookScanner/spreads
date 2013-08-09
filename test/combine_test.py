c2ac105d80fc370ec9e4b29bb03df4f077fb9bb4
Johannes Baiter <johannes.baiter@gmail.com>
2013-08-09 18:53:14 +0200

    Fix tests

--- test/combine_test.py
+++ test/combine_test.py
@@ -16,7 +16,7 @@ class TestCombine(object):
     @patch('os.listdir')
     def test_combine(self, exists, mkdir, listdir):
         exists.return_value = False
-        listdir.side_effect = (['a.jpg', 'b.jpg', 'c.jpg'],
+        listdir.side_effect = (['a.jpg', 'b.jpg', 'c.jpg', 'd.jpg'],
                                ['a.jpg', 'b.jpg', 'c.jpg', 'd.jpg'])
         combine.os.path.exists = exists
         combine.os.mkdir = mkdir
@@ -27,5 +27,23 @@ class TestCombine(object):
         out_tree = [x[0][1] for x in combine.shutil.copyfile.call_args_list]
         assert out_tree == ['/tmp/foo/raw/0000.jpg', '/tmp/foo/raw/0001.jpg',
                             '/tmp/foo/raw/0002.jpg', '/tmp/foo/raw/0003.jpg',
-                            '/tmp/foo/raw/0004.jpg', '/tmp/foo/raw/0005.jpg']
+                            '/tmp/foo/raw/0004.jpg', '/tmp/foo/raw/0005.jpg',
+                            '/tmp/foo/raw/0006.jpg', '/tmp/foo/raw/0007.jpg']
         assert combine.shutil.rmtree.call_count == 2
+
+    @patch('os.path.exists')
+    @patch('os.mkdir')
+    @patch('os.listdir')
+    def test_combine_inequal(self, exists, mkdir, listdir):
+        exists.return_value = False
+        listdir.side_effect = (['a.jpg', 'b.jpg', 'c.jpg'],
+                               ['a.jpg', 'b.jpg', 'c.jpg', 'd.jpg'])
+        combine.os.path.exists = exists
+        combine.os.mkdir = mkdir
+        combine.os.listdir = listdir
+        combine.shutil = Mock()
+        mock_cams = [Mock(), Mock()]
+        self.plug.download(mock_cams, '/tmp/foo')
+        out_tree = [x[0][1] for x in combine.shutil.copyfile.call_args_list]
+        assert out_tree == []
+        assert combine.shutil.rmtree.call_count == 0
