446b7f1821efed173ae2661ef3d2e24c0a236b9b
Johannes Baiter <johannes.baiter@gmail.com>
2013-12-11 19:13:02 +0100

    Fix cli_test for workflow object

--- test/cli_test.py
+++ test/cli_test.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from itertools import chain, repeat
 
 from mock import call, patch, MagicMock as Mock
@@ -5,6 +6,7 @@ from nose.tools import raises
 
 import spreads
 import spreads.cli as cli
+import spreads.confit as confit
 from spreads.util import DeviceException
 
 spreads.util.find_in_path = Mock(return_value=True)
@@ -12,77 +14,45 @@ spreads.util.find_in_path = Mock(return_value=True)
 
 class TestCLI(object):
     def setUp(self):
-        spreads.config.clear()
-        spreads.config.read(user=False)
-        spreads.config['plugins'] = []
-        self.plugins = [Mock(), Mock()]
-        cli.get_pluginmanager = Mock(return_value=self.plugins)
-        cli.workflow = Mock()
-        self.devices = [Mock(), Mock()]
-        self.devices[0].orientation = 'right'
-        self.devices[1].orientation = 'left'
-
-    def test_configure(self):
-        cli.getch = Mock(return_value=' ')
-        cli.get_devices = Mock(side_effect=[[x] for x in self.devices])
-        cli.configure()
-        assert cli.getch.call_count == 4
-        assert cli.get_devices.call_count == 2
-        assert self.devices[0].set_orientation.call_count == 1
-        assert self.devices[0].set_orientation.call_args_list == [call('left')]
-        assert self.devices[1].set_orientation.call_count == 1
-        assert self.devices[1].set_orientation.call_args_list == (
-            [call('right')])
-
-    @raises(DeviceException)
-    def test_configure_twodevices(self):
-        cli.getch = Mock(return_value=' ')
-        cli.get_devices = Mock(return_value=self.devices)
-        cli.configure()
-
-    @raises(DeviceException)
-    def test_configure_nodevices(self):
-        cli.getch = Mock(return_value=' ')
-        cli.get_devices = Mock(return_value=[])
-        cli.configure()
+        self.workflow = Mock()
+        self.workflow.devices = [Mock(), Mock()]
+        self.workflow.devices[0].orientation = 'right'
+        self.workflow.devices[1].orientation = 'left'
+        self.workflow.config = confit.Configuration('test_cli')
 
     def test_capture(self):
+        self.workflow.config['capture']['capture_keys'] = ["b", " "]
         cli.getch = Mock(side_effect=chain(repeat('b', 3), 'c'))
-        cli.get_devices = Mock(return_value=self.devices)
-        cli.capture()
+        cli.capture(self.workflow)
         assert cli.getch.call_count == 4
-        assert cli.workflow.prepare_capture.call_count == 1
-        assert cli.workflow.capture.call_count == 3
-        assert cli.workflow.finish_capture.call_count == 1
+        assert self.workflow.prepare_capture.call_count == 1
+        assert self.workflow.capture.call_count == 3
+        assert self.workflow.finish_capture.call_count == 1
         #TODO: stats correct?
 
     @raises(DeviceException)
     def test_capture_nodevices(self):
         cli.getch = Mock(return_value=' ')
-        cli.get_devices = Mock(return_value=[])
-        cli.capture()
+        self.workflow.devices = []
+        cli.capture(self.workflow)
 
     @raises(DeviceException)
     def test_capture_noorientation(self):
-        self.devices[0].orientation = None
+        self.workflow.devices[0].orientation = None
         cli.getch = Mock(return_value='c')
-        cli.get_devices = Mock(return_value=self.devices)
-        cli.capture()
+        cli.capture(self.workflow)
 
     def test_postprocess(self):
-        args = Mock()
-        args.path = '/tmp/foo'
-        cli.postprocess(args=args)
-        assert cli.workflow.process.call_args == call('/tmp/foo')
+        self.workflow.path = '/tmp/foo'
+        cli.postprocess(self.workflow)
+        assert self.workflow.process.call_count == 1
 
     def test_wizard(self):
-        args = Mock()
-        args.path = '/tmp/foo'
+        self.workflow.path = '/tmp/foo'
+        self.workflow.config['capture']['capture_keys'] = ["b", " "]
         cli.getch = Mock(side_effect=chain(repeat('b', 10), 'c',
                                            repeat('b', 10)))
-        cli.get_devices = Mock(side_effect=self.devices)
-        spreads.config['keep'] = False
-        cli.wizard(args, self.devices)
+        cli.wizard(self.workflow)
 
     def test_parser(self):
         cli.get_pluginmanager = Mock()
@@ -95,8 +65,15 @@ class TestCLI(object):
         # TODO: Config from args?
         # TODO: Loglevel set correctly?
         # TODO: Correct function executed?
+        cli.confit = Mock()
+        cli.Workflow = Mock(return_value=self.workflow)
+        self.workflow.config["subcommand"] = "test_cmd"
+        self.workflow.config["loglevel"] = "info"
+        self.workflow.config.dump = Mock()
+        cli.confit.LazyConfig = Mock(return_value=self.workflow.config)
+        cli.test_cmd = Mock()
+        cli.setup_plugin_config = Mock()
         cli.setup_parser = Mock()
-        cli.config.dump = Mock()
         exists.return_value = False
         cli.os.path.exists = exists
         cli.main()
