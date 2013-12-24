12660c0787b6a85dc463ad0879bd6e7042ebc97a
Johannes Baiter <johannes.baiter@gmail.com>
2013-12-24 10:21:05 +0100

    autorotate: Fix tests

--- tests/autorotate_test.py
+++ tests/autorotate_test.py
@@ -3,7 +3,7 @@ import tempfile
 import unittest
 
 import spreads.vendor.confit as confit
-from mock import call, MagicMock as Mock
+from mock import call, MagicMock as Mock, patch
 from spreads.vendor.pathlib import Path
 
 import spreads.util
@@ -26,16 +26,15 @@ class TestAutorotate(unittest.TestCase):
         test_files = [self.path / 'raw' / x
                       for x in ('000.jpg', '001.jpg', '002.jpg')]
         map(lambda x: x.touch(), test_files)
-        mock_exifs = [Mock(), Mock(), Mock()]
-        for item, orient in zip(mock_exifs, (6, 8, -1)):
-            item.exif.primary.Orientation = [orient]
-        autorotate.JpegFile = Mock()
-        autorotate.JpegFile.fromFile = Mock(side_effect=mock_exifs)
+
         mock_pool = Mock()
         autorotate.futures.ProcessPoolExecutor = Mock(return_value=mock_pool)
-        plug = autorotate.AutoRotatePlugin(self.config)
-        plug.process(self.path)
-        assert autorotate.JpegFile.fromFile.call_count == 3
+        with patch('spreadsplug.autorotate._get_exif_orientation') as geo:
+            geo.side_effect = (6, 8, -1)
+            plug = autorotate.AutoRotatePlugin(self.config)
+            plug.process(self.path)
+
+        assert geo.call_count == 3
         mock_pool.__enter__().submit.assert_has_calls([
             call(autorotate.rotate_image, test_files[0], rotation=90),
             call(autorotate.rotate_image, test_files[1], rotation=-90)
@@ -44,16 +43,14 @@ class TestAutorotate(unittest.TestCase):
     def test_process_inverse(self):
         test_files = [self.path / 'raw' / '000.jpg']
         map(lambda x: x.touch(), test_files)
-        mock_exif = Mock()
-        mock_exif.exif.primary.Orientation = [6]
-        autorotate.JpegFile = Mock()
-        autorotate.JpegFile.fromFile = Mock(return_value=mock_exif)
         mock_pool = Mock()
         autorotate.futures.ProcessPoolExecutor = Mock(return_value=mock_pool)
         self.config['autorotate']['rotate_odd'] = 90
         self.config['autorotate']['rotate_even'] = -90
         plug = autorotate.AutoRotatePlugin(self.config)
-        plug.process(self.path)
+        with patch('spreadsplug.autorotate._get_exif_orientation') as geo:
+            geo.return_value = 6
+            plug.process(self.path)
         mock_pool.__enter__().submit.assert_called_with(
             autorotate.rotate_image, test_files[0], rotation=-90
         )
@@ -63,7 +60,8 @@ class TestAutorotate(unittest.TestCase):
         mock_img.__enter__().height = 200
         mock_img.__enter__().width = 800
         autorotate.wand.image.Image = Mock(return_value=mock_img)
-        autorotate.rotate_image('foo.jpg', 90)
+        with patch('spreadsplug.autorotate.subprocess.check_output'):
+            autorotate.rotate_image('foo.jpg', 90)
         assert mock_img.__enter__().rotate.call_args_list == [call(90)]
         assert mock_img.__enter__().save.call_args_list == [
             call(filename='foo.jpg')]
