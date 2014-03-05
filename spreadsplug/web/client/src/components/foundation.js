c1c4f812ae546f5f2dad0e6a8d762b0598bb56b8
Johannes Baiter <johannes.baiter@gmail.com>
2014-03-06 00:18:31 +0100

    web: Pagination and hires lightboxes for workflow details screen

--- spreadsplug/web/client/src/components/foundation.js
+++ spreadsplug/web/client/src/components/foundation.js
@@ -57,6 +57,76 @@
                 </div>
                );
       }
+    }),
+    pagination: React.createClass({
+      handleBack: function() {
+        this.handleToPage(this.state.currentPage-1);
+      },
+      handleForward: function() {
+        this.handleToPage(this.state.currentPage+1);
+      },
+      handleToPage: function(idx) {
+        this.props.onBrowse(idx);
+        this.setState({
+          currentPage: idx
+        });
+      },
+      getInitialState: function() {
+        return {
+          currentPage: 1
+        };
+      },
+      render: function() {
+        /* TODO: Should be like this:
+         * <left> [1] ... [cur-1] [[cur]] [cur+1] ... [n] <right>
+         * <left> [1] [[2]] [3] ... [n] <right>
+         * <left> [1] ... [n-2] [[n-1]] [n] <right>
+         * <left> [[1]] [2] [3] ... [n] <right>
+         * <left> [1] ... [n-2] [n-1] [[n]] <right>
+         */
+        var pageButton = React.createClass({
+          render: function() {
+            return (
+              <li className={this.props.current ? "current": ""}>
+                <a onClick={function() {this.props.onClick(this.props.num)}.bind(this)}>
+                  {this.props.num}
+                </a>
+              </li>
+            );
+          }
+        }).bind(this);
+        var lastPage = this.props.pageCount,
+            currentPage = this.state.currentPage,
+            uncenteredPagination;
+
+        uncenteredPagination = (
+          <ul className="pagination">
+            <li className={"arrow" + (currentPage == 1 ? " unavailable" : '')}>
+              <a onClick={this.handleBack}>&laquo;</a>
+            </li>
+            <pageButton current={currentPage == 1} num={1} onClick={this.handleToPage} />
+            {(currentPage > 3) ? <li className="unavailable"><a>&hellip;</a></li>:''}
+            {lastPage > 2 ? _.range(currentPage-2, currentPage+3).map(function(idx) {
+              if (idx <= 1 || idx >= lastPage) return;
+              return <pageButton current={currentPage == idx} num={idx} onClick={this.handleToPage} />;
+            }.bind(this)):''}
+            {(currentPage < lastPage-2) ? <li className="unavailable"><a>&hellip;</a></li>:''}
+            <pageButton current={currentPage == lastPage} num={lastPage} onClick={this.handleToPage} />
+            <li className={"arrow" + (currentPage == lastPage ? " unavailable" : '')}>
+              <a onClick={this.handleForward}>&raquo;</a>
+            </li>
+          </ul>
+        );
+        if (this.props.centered) {
+          return (
+            <div className="pagination-centered">
+              {uncenteredPagination}
+            </div>
+          );
+        } else {
+          return uncenteredPagination;
+        }
+      }
     })
-  };
+  }
 }());
--- spreadsplug/web/client/src/components/workflow.js
+++ spreadsplug/web/client/src/components/workflow.js
@@ -6,18 +6,49 @@
   var React = require('react/addons'),
       ModelMixin = require('../workflow.js'),
       foundation = require('./foundation.js'),
+      lightbox = require('./lightbox.js'),
       row = foundation.row,
-      column = foundation.column;
+      column = foundation.column,
+      pagination = foundation.pagination;
 
   module.exports = React.createClass({
     mixins: [ModelMixin],
     getBackboneModels: function() {
       return [this.props.workflow];
     },
+    getInitialState: function() {
+      return {
+        thumbStart: 0,
+        thumbCount: 24,
+        lightboxImage: undefined
+      };
+    },
+    openLightbox: function(img) {
+      this.setState({
+        lightboxImage: img
+      });
+    },
+    closeLightbox: function() {
+      this.setState({
+        lightboxImage: undefined
+      });
+    },
+    browse: function(pageIdx) {
+      this.setState({
+        thumbStart: (pageIdx-1)*this.state.thumbCount
+      });
+    },
     render: function() {
-      var workflow = this.props.workflow;
+      var workflow = this.props.workflow,
+          pageCount = (workflow.get('images').length / this.state.thumbCount) | 0,
+          thumbStart = this.state.thumbStart,
+          thumbStop = this.state.thumbStart+this.state.thumbCount;
       return (
         <main>
+          {this.state.lightboxImage ?
+            <lightbox onClose={this.closeLightbox} src={this.state.lightboxImage} />:
+            ''
+          }
           <row>
             <column size='12'>
               <h1>{workflow.get('name')}</h1>
@@ -36,34 +67,39 @@
               </ul>
             </column>
           </row>
+          {workflow.has('images') && workflow.get('images') ?
           <row>
             <column size='12'>
               <h2>Captured images</h2>
               <ul className="small-block-grid-8">
-                {workflow.has('images') ?
-                  workflow.get('images').map(function(image) {
+                {workflow.get('images').slice(thumbStart, thumbStop).map(function(image) {
                     return (
-                      <li key={image}><a className="th"><img src={image + '/thumb'} /></a></li>
+                      <li key={image}>
+                        <a className="th" onClick={function(){this.openLightbox(image);}.bind(this)}>
+                          <img src={image + '/thumb'} />
+                        </a>
+                      </li>
                     );
-                  })
-                  :''}
+                  }.bind(this))}
               </ul>
+              {pageCount > 1 ? <pagination centered={true} pageCount={pageCount} onBrowse={this.browse} />:''}
             </column>
           </row>
+          :''}
+          {workflow.has('output_files') && workflow.get('output_files').length ?
           <row>
             <column size='12'>
               <h2>Output files</h2>
               <ul>
-                {workflow.has('output_files') ?
-                  workflow.get('output_files').map(function(outFile) {
+                {workflow.get('output_files').map(function(outFile) {
                     return (
                       <li key={outFile}><a href={outFile}>{outFile}</a></li>
                     );
-                  })
-                  :''}
+                  })}
               </ul>
             </column>
           </row>
+          :''}
         </main>
       );
     }
