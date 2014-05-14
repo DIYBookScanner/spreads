53efd2adf26659c38c1b7d7f35c8e4dbf4ea57c5
Johannes Baiter <johannes.baiter@gmail.com>
2014-05-14 09:18:51 +0200

    web: Fix configuration dialogue

--- spreadsplug/web/client/src/events.js
+++ spreadsplug/web/client/src/events.js
@@ -21,70 +21,70 @@
   var Backbone = require('backbone'),
       _ = require('underscore'),
       jQuery = require('jquery'),
-      websocket,
-      eventDispatcher;
+      websocket, EventDispatcher;
 
-  eventDispatcher = _.clone(Backbone.Events);
-
-  function emitEvent(event) {
-    var parts = event.name.split(':')
-    if (parts[0] === 'workflow:' && parts[1] !== 'created') {
-        event.name = parts[0] + event.data.id + ':' + parts[1];
-        delete event.data['id'];
+  EventDispatcher = function() {
+    if (window.MozWebSocket) {
+      window.WebSocket = window.MozWebSocket;
     }
-    eventDispatcher.trigger(event.name, event.data);
-  }
-
-  function longPoll() {
-    jQuery.ajax({
-      url: "/api/poll",
-      success: function(data){
-        _.each(data, emitEvent);
-      },
-      dataType: "json",
-      complete: function(xhr, status) {
-        if (_.contains(["timeout", "success"], status)) {
-          // Restart polling
+    if (window.WebSocket) {
+      // Try to use WebSockets
+      var hostName = window.location.hostname,
+          port = parseInt(window.location.port)+1;
+      websocket = new WebSocket("ws://" + hostName + ":" + port);
+      websocket.onclose = function() {
+        if (!websocket.onmessage) {
+          // This means we were never able to establish a connection,
+          // probably because the websocket server is not accessible, so
+          // we fall back to long-polling.
+          console.warn("Could not open connection to WebSocket server " +
+                        "at " + websocket.url + ", falling back to " +
+                        "long polling.");
           longPoll();
-        } else {
-          // Back off for 30 seconds before polling again
-          _.delay(longPoll, 30*1000);
         }
-      },
-      timeout: 30*1000  // Cancel the request after 30 seconds
-    });
-  }
+      }.bind(this);
+      websocket.onopen = function() {
+        // Start listening to server events
+        websocket.onmessage = function(messageEvent) {
+          this.emitEvent(JSON.parse(messageEvent.data));
+        }.bind(this);
+      }.bind(this);
+    } else {
+      // Use AJAX long-polling as a fallback when WebSockets are not supported
+      // by the browser
+      longPoll();
+    }
+  };
 
-  if (window.MozWebSocket) {
-    window.WebSocket = window.MozWebSocket;
-  }
-  if (window.WebSocket) {
-    // Try to use WebSockets
-    var hostName = window.location.hostname,
-        port = parseInt(window.location.port)+1;
-    websocket = new WebSocket("ws://" + hostName + ":" + port);
-    websocket.onclose = function() {
-      if (!websocket.onmessage) {
-        // This means we were never able to establish a connection,
-        // probably because the websocket server is not accessible, so
-        // we fall back to long-polling.
-        console.warn("Could not open connection to WebSocket server " +
-                      "at " + websocket.url + ", falling back to " +
-                      "long polling.");
-        longPoll();
+  _.extend(EventDispatcher.prototype, Backbone.Events, {
+    emitEvent: function emitEvent(event) {
+      var parts = event.name.split(':')
+      if (parts[0] === 'workflow:' && parts[1] !== 'created') {
+          event.name = parts[0] + event.data.id + ':' + parts[1];
+          delete event.data['id'];
       }
-    }.bind(this);
-    websocket.onopen = function() {
-      // Start listening to server events
-      websocket.onmessage = function(messageEvent) {
-        emitEvent(JSON.parse(messageEvent.data));
-      };
-    }.bind(this);
-  } else {
-    // Use AJAX long-polling as a fallback when WebSockets are not supported
-    // by the browser
-    longPoll();
-  }
+      this.trigger(event.name, event.data);
+    },
+    longPoll: function longPoll() {
+      jQuery.ajax({
+        url: "/api/poll",
+        success: function(data){
+          _.each(data, this.emitEvent, this);
+        }.bind(this),
+        dataType: "json",
+        complete: function(xhr, status) {
+          if (_.contains(["timeout", "success"], status)) {
+            // Restart polling
+            longPoll();
+          } else {
+            // Back off for 30 seconds before polling again
+            _.delay(longPoll, 30*1000);
+          }
+        },
+        timeout: 30*1000  // Cancel the request after 30 seconds
+      });
+    }
+  });
 
-  module.exports = eventDispatcher;
+  module.exports = EventDispatcher;
 }());
