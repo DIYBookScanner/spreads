72547e1dbd68a897dd1469ffbbb37d65e8f0efae
Johannes Baiter <johannes.baiter@gmail.com>
2014-06-21 11:43:06 +0200

    Update scantailor plugin to new API

--- spreadsplug/scantailor.py
+++ spreadsplug/scantailor.py
@@ -72,10 +72,7 @@ class ScanTailorPlugin(HookPlugin, ProcessHookMixin):
                               subprocess.check_output('scantailor-cli')
                               .splitlines()[7]))
 
-    def _generate_configuration(self, projectfile, img_dir, out_dir):
-        if not out_dir.exists():
-            out_dir.mkdir()
-        logger.info("Generating ScanTailor configuration")
+    def _generate_configuration(self, in_paths, projectfile, out_dir):
         filterconf = [self.config[x].get(bool)
                       for x in ('rotate', 'split_pages', 'deskew', 'content',
                                 'auto_margins')]
@@ -101,37 +98,33 @@ class ScanTailorPlugin(HookPlugin, ProcessHookMixin):
                 '--margins-bottom={0}'.format(marginconf[2]),
                 '--margins-left={0}'.format(marginconf[3]),
             ])
-        if self._enhanced:
-            generation_cmd.append(unicode(img_dir))
-        else:
-            generation_cmd.extend([unicode(x)
-                                   for x in sorted(img_dir.iterdir())])
+        generation_cmd.extend(in_paths)
         generation_cmd.append(unicode(out_dir))
         logger.debug(" ".join(generation_cmd))
         proc = psutil.Process(subprocess.Popen(generation_cmd).pid)
 
-        num_images = sum(1 for x in img_dir.iterdir())
+        num_images = len(in_paths)
         num_steps = (end_filter - start_filter)+1
-        last_filenum = 0
-        recent_filenum = 0
+        last_fileidx = 0
+        recent_fileidx = 0
         finished_steps = 0
         while proc.is_running():
             try:
-                recent_filenum = next(int(Path(x.path).name.split('.')[0])
+                recent_fileidx = next(in_paths.index(x.path)
                                       for x in proc.open_files()
-                                      if unicode(img_dir) in x.path)
+                                      if x.path in in_paths)
             except StopIteration:
                 pass
             except psutil.AccessDenied:
                 # This means the process is no longer running
                 break
-            if recent_filenum == last_filenum:
-                time.sleep(.1)
+            if recent_fileidx == last_fileidx:
+                time.sleep(.01)
                 continue
-            if recent_filenum < last_filenum:
+            if recent_fileidx < last_fileidx:
                 finished_steps += 1
-            last_filenum = recent_filenum
-            progress = 0.5*((finished_steps*num_images+last_filenum) /
+            last_fileidx = recent_fileidx
+            progress = 0.5*((finished_steps*num_images+last_fileidx) /
                             float(num_steps*num_images))
             self.on_progressed.send(self, progress=progress)
 
@@ -181,27 +174,54 @@ class ScanTailorPlugin(HookPlugin, ProcessHookMixin):
             for p in processes[:]:
                 if p.poll() is not None:
                     processes.remove(p)
+            time.sleep(.01)
         shutil.rmtree(unicode(temp_dir))
 
-    def process(self, path):
+    def process(self, pages, target_path):
         autopilot = self.config['autopilot'].get(bool)
         if not autopilot and not find_in_path('scantailor'):
             raise MissingDependencyException(
                 "Could not find executable `scantailor` in"
                 " $PATH. Please install the appropriate"
                 " package(s)!")
-        projectfile = path / "{0}.ScanTailor".format(path.name)
-        img_dir = path / 'data' / 'raw'
-        out_dir = path / 'data' / 'done'
 
-        if not projectfile.exists():
-            self._generate_configuration(projectfile, img_dir, out_dir)
+        # Create temporary files/directories
+        projectfile = Path(tempfile.mkstemp(suffix='.ScanTailor')[1])
+        out_dir = Path(tempfile.mkdtemp(prefix='st-out'))
+
+        # Map input paths to their pages so we can more easily associate
+        # the generated output files with their pages later on
+        in_paths = {}
+        for page in pages:
+            fpath = page.get_latest_processed(image_only=True)
+            if fpath is None:
+                fpath = page.raw_image
+            in_paths[unicode(fpath)] = page
+
+        logger.info("Generating ScanTailor configuration")
+        self._generate_configuration(sorted(in_paths.keys()),
+                                     projectfile, out_dir)
 
         if not autopilot:
             logger.info("Opening ScanTailor GUI for manual adjustment")
             subprocess.call(['scantailor', unicode(projectfile)])
-        logger.info("Generating output images from ScanTailor configuration.")
 
-        num_pages = sum(1 for x in img_dir.iterdir()
-                        if x.suffix.lower() in ('.jpeg', '.jpg'))
-        self._generate_output(projectfile, out_dir, num_pages)
+        logger.info("Generating output images from ScanTailor configuration.")
+        self._generate_output(projectfile, out_dir, len(pages))
+
+        # Associate generated output files with our pages
+        for fname in out_dir.glob('*.tif'):
+            out_stem = fname.stem
+            for in_path, page in in_paths.iteritems():
+                if Path(in_path).stem == out_stem:
+                    target_fname = target_path/fname.name
+                    shutil.copyfile(unicode(fname), unicode(target_fname))
+                    page.processed_images[self.__name__] = target_fname
+                    break
+            else:
+                logger.warn("Could not find page for output file {0}"
+                            .format(fname))
+
+        # Remove temporary files/directories
+        shutil.rmtree(unicode(out_dir))
+        projectfile.unlink()
