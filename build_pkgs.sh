66a438adc0ae303037250e40b6edb2a61f42b2dc
Johannes Baiter <johannes.baiter@gmail.com>
2014-07-05 15:12:50 +0200

    Fix debian build script

--- build_pkgs.sh
+++ build_pkgs.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
-VERSION=$(grep "__version__" spreads/__init__.py |sed -e "s/.* = \"\(.*\)\"/\1/g")
+GIT_VERSION=$(head -n 1 debian/changelog | sed -e "s/^spreads (\(.*\)-.*) unstable; urgency=low/\1/g")
+PY_VERSION=$(grep "__version__" spreads/__init__.py |sed -e "s/.* = \"\(.*\)\"/\1/g")
 
 mkdir -p debian_build
+rm -rf dist/
 python setup.py sdist
-cp dist/spreads-$VERSION.tar.gz debian_build/spreads_$VERSION.orig.tar.gz
-tar xf dist/spreads-$VERSION.tar.gz -C debian_build
-cp -R debian debian_build/spreads-$VERSION
-cd debian_build/spreads-$VERSION
+cp dist/spreads-$PY_VERSION.tar.gz debian_build/spreads_$GIT_VERSION.orig.tar.gz
+mkdir debian_build/spreads-$GIT_VERSION
+tar xf dist/spreads-$PY_VERSION.tar.gz -C debian_build/spreads-$GIT_VERSION --strip-components 1
+cp -R debian debian_build/spreads-$GIT_VERSION
+cd debian_build/spreads-$GIT_VERSION
 debuild -S -us -uc
 cd ../../
 mv debian_build/*dsc debian_build/*.orig.tar.gz debian_build/*.debian.tar.* dist/
