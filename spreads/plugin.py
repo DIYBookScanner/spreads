fd06795f18e6e891685de0a1712d8a8e03849d33
Johannes Baiter <johannes.baiter@gmail.com>
2014-09-25 09:09:44 +0200

    Docstrings for spreads.plugin

--- spreads/plugin.py
+++ spreads/plugin.py
@@ -15,6 +15,11 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""
+Public plugin API (realized through a range of abstract classes) and utility
+functions for enumerating and loading plugins.
+"""
+
 from __future__ import division, unicode_literals
 
 import abc
@@ -30,27 +35,31 @@ from spreads.util import (abstractclassmethod, DeviceException,
 
 
 logger = logging.getLogger("spreads.plugin")
+
+# Global cache of devices and extensions
 devices = None
 extensions = dict()
 
 
 class ExtensionException(Exception):
+    """" Raised when something went wrong during plugin enumeration/ or
+         instantiation.
+    """
     def __init__(self, message=None, extension=None):
         super(ExtensionException, self).__init__(message)
         self.extension = extension
 
 
 class SpreadsPlugin(object):  # pragma: no cover
-    """ Plugin base class.
-
-    """
+    """ Plugin base class. """
     signals = Namespace()
     on_progressed = signals.signal('plugin:progressed', doc="""\
-    Sent by a :class:`SpreadsPlugin` when it has progressed in a long-running
-    operation.
+    Sent by a :py:class:`SpreadsPlugin` when it has progressed in a
+    long-running operation.
 
-    :argument :class:`SpreadsPlugin`:   the SpreadsPlugin that progressed
-    :keyword float progress:            the progress as a value between 0 and 1
+    :argument :py:class:`SpreadsPlugin`:    the SpreadsPlugin that progressed
+    :keyword float progress:                the progress as a value between 0
+                                            and 1
     """)
 
     @classmethod
@@ -72,19 +81,19 @@ class SpreadsPlugin(object):  # pragma: no cover
                                 selectable=True),
           }
 
-        :return: dict with unicode: OptionTemplate(value, docstring, selection)
+        :returns:   dict with `unicode` ->
+                    :py:class:`spreads.config.OptionTemplate`
         """
         pass
 
     def __init__(self, config):
         """ Initialize the plugin.
 
-        :param config: The global configuration object, by default only the
-                       section with plugin-specific values gets stored in
-                       the `config` attribute, if the plugin has a `__name__`
-                       attribute.
-        :type config: confit.ConfigView
-
+        :param config: The global configuration object. If the plugin has a
+                        `__name__` attribute, only the section with
+                        plugin-specific values gets stored in the `config`
+                        attribute
+        :type config: :py:class:`confit.ConfigView`
         """
         if hasattr(self, '__name__'):
             self.config = config[self.__name__]
@@ -93,6 +102,11 @@ class SpreadsPlugin(object):  # pragma: no cover
 
 
 class DeviceFeatures(object):  # pragma: no cover
+    """ Pseudo-enum that provides various constants that
+        :py:class:`DeviceDriver` implementations can expose in their
+        :py:attr:`DeviceDriver.features` tuple to declare support for one or
+        more given features.
+    """
     #: Device can grab a preview picture
     PREVIEW = 'PREVIEW'
 
@@ -118,6 +132,9 @@ class DeviceDriver(SpreadsPlugin):  # pragma: no cover
 
     @classmethod
     def configuration_template(cls):
+        """ Returns some pre-defined options when the implementing devices
+            has the :py:attr:`DeviceFeatures.IS_CAMERA` feature.
+        """
         if DeviceFeatures.IS_CAMERA in cls.features:
             return {
                 "parallel_capture": OptionTemplate(
@@ -142,10 +159,13 @@ class DeviceDriver(SpreadsPlugin):  # pragma: no cover
 
     @abstractclassmethod
     def yield_devices(cls, config):
-        """ Search for usable devices, yield one at a time
+        """ Search for usable devices, return a generator that yields them one
+        at a time as instances of the implementing class.
 
         :param config:  spreads configuration
-        :type config:   spreads.confit.ConfigView
+        :type config:   :py:class:`spreads.confit.ConfigView`
+        :returns:       Instantiated device objects
+        :rtype:         Type of implementing class
         """
         raise NotImplementedError
 
@@ -153,10 +173,9 @@ class DeviceDriver(SpreadsPlugin):  # pragma: no cover
         """ Set connection information and other properties.
 
         :param config:  spreads configuration
-        :type config:   spreads.confit.ConfigView
+        :type config:   :py:class:`spreads.confit.ConfigView`
         :param device:  USB device to use for the object
-        :type device:   `usb.core.Device <http://github.com/walac/pyusb>`_
-
+        :type device:   py:class:`usb.core.Device`
         """
         self.config = config
         self._device = device
@@ -166,7 +185,6 @@ class DeviceDriver(SpreadsPlugin):  # pragma: no cover
         """ Check if the device is still connected.
 
         :rtype:     bool
-
         """
         raise NotImplementedError
 
@@ -174,8 +192,7 @@ class DeviceDriver(SpreadsPlugin):  # pragma: no cover
         """ Set the device target page, if applicable.
 
         :param target_page: The target page
-        :type target_page:  unicode in (u"odd", u"even")
-
+        :type target_page:  unicode, one of `odd` or `even`
         """
         raise NotImplementedError
 
@@ -183,13 +200,9 @@ class DeviceDriver(SpreadsPlugin):  # pragma: no cover
     def prepare_capture(self):
         """ Prepare device for scanning.
 
-        What this means exactly is up to the implementation and the type,
+        What this means exactly is up to the implementation and the type
         of device, usually it involves things like switching into record
-        mode, path and applying all relevant settings.
-
-        :param path:    Project base path
-        :type path:     pathlib.Path
-
+        mode and applying all relevant settings.
         """
         raise NotImplementedError
 
@@ -198,7 +211,7 @@ class DeviceDriver(SpreadsPlugin):  # pragma: no cover
         """ Capture a single image with the device.
 
         :param path:    Path for the image
-        :type path:     pathlib.Path
+        :type path:     :py:class:`pathlib.Path`
 
         """
         raise NotImplementedError
@@ -208,13 +221,16 @@ class DeviceDriver(SpreadsPlugin):  # pragma: no cover
         """ Tell device to finish capturing.
 
         What this means exactly is up to the implementation and the type of
-        device, with a camera it could e.g. involve retractingthe lense.
+        device, with a camera it could e.g. involve retracting the lense.
         """
         raise NotImplementedError
 
     @abc.abstractmethod
     def update_configuration(self, updated):
         """ Update the device configuration.
+
+        The implementing device driver should propagate these updates to the
+        hardware and make sure everything is applied correctly.
 
         :param updated:     Updated configuration values
         :type updated:      dict
@@ -225,35 +241,39 @@ class DeviceDriver(SpreadsPlugin):  # pragma: no cover
 class HookPlugin(SpreadsPlugin):
     """ Base class for HookPlugins.
 
-    Implement one of the available mixin classes to register for the
-    appropriate hooks.
-
+    Implement one of the available mixin classes
+    (:py:class:`SubcommandHooksMixin`, :py:class:`CaptureHooksMixin`,
+    py:class:`TriggerHooksMixin`, :py:class:`ProcessHooksMixin`,
+    :py:class:`OutputHooksMixin`) to register for the appropriate hooks.
     """
     pass
 
 
 class SubcommandHooksMixin(object):
+    """ Mixin for plugins that want to provide custom subcommands. """
     __metaclass__ = abc.ABCMeta
 
     @abstractclassmethod
     def add_command_parser(cls, rootparser, config):
         """ Allows a plugin to register a new command with the command-line
-            parser. The subparser that is added to :param rootparser: should
-            set the class' ``__call__`` method as the ``func`` (via
-            ``set_defaults``) that is executed when the subcommand is specified
-            on the CLI.
+            parser.
+
+        The subparser that is added to :param rootparser: should set the class'
+        ``__call__`` method as the ``func`` (via
+        :py:meth:`argparse.ArgumentParser.set_defaults`) that is executed
+        when the subcommand is specified on the CLI.
 
         :param rootparser: The root parser that this plugin should add a
                            subparser to.
-        :type rootparser:  argparse.ArgumentParser
+        :type rootparser:  :py:class:`argparse.ArgumentParser`
         :param config:     The application configuration
-        :type config:      Configuration
-
+        :type config:      :py:class:`spreads.config.Configuration`
         """
         pass
 
 
 class CaptureHooksMixin(object):
+    """ Mixin for plugins that want to hook into the capture process. """
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
@@ -281,15 +301,17 @@ class CaptureHooksMixin(object):
         """ Perform some action after capturing has finished.
 
         :param devices:     The devices used for capturing
-        :type devices:      list(DevicePlugin)
-        :param path:        Project path
-        :type path:         pathlib.Path
-
+        :type devices:      list of :py:class:`DeviceDriver`
+        :param path:        Workflow path
+        :type path:         :py:class:`pathlib.Path`
         """
         pass
 
 
 class TriggerHooksMixin(object):
+    """ Mixin for plugins that want to provice customized ways of triggering
+        a capture.
+    """
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
@@ -297,22 +319,20 @@ class TriggerHooksMixin(object):
         """ Start a thread that runs an event loop and periodically triggers
             a capture by calling the `capture_callback`.
 
-        :param capture_callback:    The function to run for triggering a
-                                    capture
+        :param capture_callback:    The function that triggers a capture
         :type capture_callback:     function
-
         """
         pass
 
     @abc.abstractmethod
     def stop_trigger_loop(self):
-        """ Stop the thread started by `start_trigger_loop*.
-
-        """
+        """ Stop the thread started by :py:meth:`start_trigger_loop`. """
         pass
 
 
 class ProcessHooksMixin(object):
+    """ Mixin for plugins that want to provide postprocessing functionality.
+    """
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
@@ -321,15 +341,15 @@ class ProcessHooksMixin(object):
             or generate a different output.
 
         :param pages:       Pages to be processed
-        :type pages:        list of Page objects
+        :type pages:        list of :py:class:`spreads.workflow.Page`
         :param target_path: Target directory for processed files
-        :type target_path:  pathlib.Path
-
+        :type target_path:  :py:class:`pathlib.Path`
         """
         pass
 
 
 class OutputHooksMixin(object):
+    """ Mixin for plugins that want to create output files. """
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
@@ -337,30 +357,43 @@ class OutputHooksMixin(object):
         """ Assemble an output file from the pages.
 
         :param pages:       Project path
-        :type pages:        list of Page objects
+        :type pages:        list of :py:class:`spreads.workflow.Page`
         :param target_path: Target directory for processed files
-        :type target_path:  pathlib.Path
+        :type target_path:  :py:class:`pathlib.Path`
         :param metadata:    Metadata for workflow
-        :type metadata:     bagit.BagInfo
+        :type metadata:     :py:class:`spreads.metadata.Metadata`
         :param table_of_contents: Table of Contents for workflow
-        :type table_of_contents: list(TocEntry)
-
+        :type table_of_contents: list of :py:class:`spreads.workflow.TocEntry`
         """
         pass
 
 
 def available_plugins():
+    """ Get the names of all installed plugins.
+
+    :returns:    List of plugin names
+    """
     return sorted([ext.name for ext in
                    pkg_resources.iter_entry_points('spreadsplug.hooks')])
 
 
 def get_plugins(*names):
+    """ Get instantiated and configured plugin instances.
+
+    :param names:   One or more plugin names
+    :type names:    unicode
+    :returns:       Mapping of plugin name to plugin instance
+    :rtype:         dict of unicode -> :py:class:`SpreadsPlugin`
+    """
+    # Reference to global extension cache
     global extensions
     plugins = OrderedDict()
     for name in names:
+        # Already in cache?
         if name in extensions:
             plugins[name] = extensions[name]
             continue
+        # Nope, so let's instantiate it...
         try:
             logger.debug("Looking for extension \"{0}\"".format(name))
             ext = next(pkg_resources.iter_entry_points('spreadsplug.hooks',
@@ -371,6 +404,7 @@ def get_plugins(*names):
         try:
             plugin = ext.load()
             plugins[name] = plugin
+            # ... and put it into the cache
             extensions[name] = plugin
         except ImportError as err:
             message = err.message
@@ -387,11 +421,22 @@ def get_plugins(*names):
 
 
 def available_drivers():
+    """ Get the names of all installed device drivers.
+
+    :returns:    List of driver names
+    """
     return [ext.name
             for ext in pkg_resources.iter_entry_points('spreadsplug.devices')]
 
 
 def get_driver(driver_name):
+    """ Get a device driver.
+
+    :param driver_name: Name of driver to instantiate
+    :type driver_name:  unicode
+    :returns:           The driver class
+    :rtype:             :py:class:`DeviceDriver` class
+    """
     try:
         ext = next(pkg_resources.iter_entry_points('spreadsplug.devices',
                                                    name=driver_name))
@@ -407,8 +452,16 @@ def get_driver(driver_name):
 
 
 def get_devices(config, force_reload=False):
-    """ Initialize configured devices.
+    """ Get initialized and configured device instances.
+
+    :param config:          Global configuration
+    :type config:           :py:class:`spreads.config.Configuration`
+    :param force_reload:    Don't load devices from cache
+    :type force_reload:     bool
+    :return:                Device instances
+    :rtype:                 list of :py:class:`DeviceDriver` objects
     """
+    # Reference to global device cache
     global devices
     if not devices or force_reload:
         if 'driver' not in config.keys():
