4c22af26eb65cf308644375d1defabc5f0e70fa7
Johannes Baiter <johannes.baiter@gmail.com>
2014-10-01 18:24:16 +0200

    hidtrigger: Add docstrings

--- spreadsplug/hidtrigger.py
+++ spreadsplug/hidtrigger.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+""" Trigger plugin that waits for a key press on any USB HID device. """
+
 import hidapi
 import logging
 import threading
@@ -31,6 +33,11 @@ class HidTrigger(HookPlugin, TriggerHooksMixin):
     _exit_event = None
 
     def __init__(self, config):
+        """ Find all attached USB HID devices and memorize them for later.
+
+        :param config:  The workflow's configuration
+        :type config:   :py:class:`confit.ConfigView`
+        """
         self._logger = logging.getLogger('spreadsplug.hidtrigger')
         self._logger.debug("Initializing HidTrigger plugin")
         self._hid_devs = []
@@ -41,6 +48,11 @@ class HidTrigger(HookPlugin, TriggerHooksMixin):
             self._logger.warning("Could not find any HID devices.")
 
     def start_trigger_loop(self, capture_callback):
+        """ Launch the triggering loop in a background thread.
+
+        :param capture_callback:    Callback for triggering a capture
+        :type capture_callback:     function
+        """
         if not self._hid_devs:
             self._logger.warning("Not starting trigger loop since there are "
                                  "no HID devices.")
@@ -53,6 +65,7 @@ class HidTrigger(HookPlugin, TriggerHooksMixin):
         self._loop_thread.start()
 
     def stop_trigger_loop(self):
+        """ Stop the triggering loop and its thread. """
         if self._exit_event is None:
             # Return if no loop thread is running
             return
@@ -61,8 +74,12 @@ class HidTrigger(HookPlugin, TriggerHooksMixin):
         self._loop_thread.join()
 
     def _trigger_loop(self, capture_func):
-        # Polls all attached HID devices for a press->release event and
-        # trigger a capture.
+        """ Poll all USB HID devices for a press->release event and trigger a
+            capture.
+
+        :param capture_func:    Callback for triggering a capture
+        :type capture_func:     function
+        """
         while not self._exit_event.is_set():
             for dev in self._hid_devs:
                 # See if there's input
@@ -81,6 +98,11 @@ class HidTrigger(HookPlugin, TriggerHooksMixin):
                     time.sleep(0.01)
 
     def _find_devices(self):
+        """ Find all attached USB HID devices.
+
+        :returns:   All devices found
+        :rtype:     Generator that yields :py:class:`hidapi.Device`
+        """
         for candidate in hidapi.enumerate():
             try:
                 dev = hidapi.Device(candidate, blocking=False)
