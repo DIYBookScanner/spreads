b40730c85ca29de444aeb4d90715583e5ec5af0a
Johannes Baiter <johannes.baiter@gmail.com>
2014-10-10 23:31:19 +0200

    Add license to spreadsplug.__init__

--- spreadsplug/__init__.py
+++ spreadsplug/__init__.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2014 Johannes Baiter <johannes.baiter@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 spreadsplug package
 
@@ -47,7 +64,7 @@ In alphabetical order:
     Postprocessing plugin to perform optical character recognition on the
     images, using the `tesseract` application
 
-:Py:mod:`spreadsplug.web`
+:py:mod:`spreadsplug.web`
     Subcommand plugin for a RESTful HTTP API (implemented with Flask and
     Tornado) and a single-page JavaScript web application (implemented with
     ReactJS)
