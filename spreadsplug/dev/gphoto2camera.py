95dbb4187661aeaa6260d887389454bc570b5a63
Johannes Baiter <johannes.baiter@gmail.com>
2014-11-18 23:56:12 +0100

    gphoto2camera: Use gphoto2-cffi instead of piggyphoto

--- spreadsplug/dev/gphoto2camera.py
+++ spreadsplug/dev/gphoto2camera.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 import logging
-import os
-import tempfile
 
 from jpegtran import JPEGImage
 
 from spreads.config import OptionTemplate
 from spreads.plugin import DeviceDriver, DeviceFeatures
 
-import piggyphoto as pp
+import gphoto2
 
 
 class GPhoto2CameraDevice(DeviceDriver):
@@ -21,6 +19,17 @@ class GPhoto2CameraDevice(DeviceDriver):
 
     @classmethod
     def configuration_template(cls):
+        # TODO: The basic problem that prevents us from supporting more
+        # configuration values is that in gphoto2, the available options
+        # are only known when the device is already initialized, which
+        # is not possible since this is ia @classmethod.
+        # We also cannot have the user specify a specific gphoto2 driver,
+        # since at the time this method is called, not even the configuration
+        # is loaded.
+        # The best way to proceed is probably to determine a superset of
+        # configuration values that are of interest to spreads users and
+        # map from those to the driver's specific options.
+        # At first we will likely only support ptp2
         conf = super(GPhoto2CameraDevice, cls).configuration_template()
         conf.update(
             {'iso': OptionTemplate(u"Auto", "The ISO value"),
@@ -40,14 +49,7 @@ class GPhoto2CameraDevice(DeviceDriver):
         :type config:   spreads.config.ConfigView
 
         """
-        pil = pp.portInfoList()
-        for name, path in pp.cameraList(autodetect=True).toList():
-            pi = pil.get_info(pil.lookup_path(path))
-            logger = logging.getLogger('GPhoto2Camera')
-            logger.debug("Connecting to camera {} at {}".format(name, path))
-            cam = pp.camera(autoInit=False)
-            cam.port_info = pi
-            cam.init()
+        for cam in gphoto2.list_cameras():
             yield cls(config, cam)
 
     def __init__(self, config, camera):
@@ -56,7 +58,7 @@ class GPhoto2CameraDevice(DeviceDriver):
         :param config:  spreads configuration
         :type config:   spreads.confit.ConfigView
         :param camera:  camera device
-        :type camera:   `camera <http://github.com/piggyphoto>`
+        :type camera:   :py:class:`gphoto2.Camera`
 
         """
         self.logger = logging.getLogger('GPhoto2Camera')
@@ -64,8 +66,8 @@ class GPhoto2CameraDevice(DeviceDriver):
         self.config = config
 
         try:
-            serialnumber = self._camera.config['status']['serialnumber'].value
-            self.target_page = config['target_page'][serialnumber].get()
+            self._serial_number = self._camera.status.serialnumber
+            self.target_page = config['target_page'][self._serial_number].get()
         except:
             self.target_page = None
 
@@ -74,13 +76,8 @@ class GPhoto2CameraDevice(DeviceDriver):
 
     def connected(self):
         try:
-            # check if we're connected by accessing the config, which is
-            # backed by a getter function that attempts to retrieve the
-            # config from the camera (and throws libgphoto2error if unable
-            # to communicate with it).
-            self._camera.config
-            return True
-        except pp.libgphoto2error:
+            return self._camera.status is not None
+        except gphoto2.errors.GPhoto2Error:
             return False
 
     def set_target_page(self, target_page):
@@ -93,8 +90,7 @@ class GPhoto2CameraDevice(DeviceDriver):
         # Map this in the spreads config keyed by the camera serial number.
         # We can't use the approach taken by CHDKCameraDevice, because most
         # PTP cameras don't support writing files via PTP.
-        serialnumber = self._camera.config['status']['serialnumber'].value
-        self.config['target_page'][serialnumber].set(target_page)
+        self.config['target_page'][self._serialnumber].set(target_page)
 
     def prepare_capture(self):
         iso = str(self.config['iso'].get())
@@ -103,48 +99,41 @@ class GPhoto2CameraDevice(DeviceDriver):
         aperture = str(self.config['aperture'].get())
 
         cfg = self._camera.config
-        mode = cfg['capturesettings']['autoexposuremode']
+        mode = cfg['capturesettings']['autoexposuremode'].value
 
         self.logger.debug("Camera mode: {}".format(mode))
         self.logger.debug(
             "Setting iso={}, shutter_speed={}, aperture={}, raw={}"
             .format(iso, shutter_speed, aperture, shoot_raw))
 
-        cfg['imgsettings']['iso'].value = iso
+        cfg['imgsettings']['iso'].set(iso)
 
         if shoot_raw:
             format = 'RAW'
         else:
             format = 'Large Fine JPEG'
-        cfg['imgsettings']['imageformat'].value = format
-        cfg['imgsettings']['imageformatsd'].value = format
+        cfg['imgsettings']['imageformat'].set(format)
+        cfg['imgsettings']['imageformatsd'].set(format)
 
         if cfg['capturesettings']['shutterspeed']:
-            cfg['capturesettings']['shutterspeed'].value = shutter_speed
+            cfg['capturesettings']['shutterspeed'].set(shutter_speed)
         else:
             self.logger.debug(
                 "Skipping shutter_speed config due to camera mode ({})"
                 .format(mode))
 
         if cfg['capturesettings']['aperture']:
-            cfg['capturesettings']['aperture'].value = aperture
+            cfg['capturesettings']['aperture'].set(aperture)
         else:
             self.logger.debug(
                 "Skipping aperture config due to camera mode ({})"
                 .format(mode))
 
-        self._camera.config = cfg
-
     def finish_capture(self):
         pass
 
     def get_preview_image(self):
-        fpath = tempfile.mkstemp()[1]
-        self._camera.capture_preview(fpath)
-        with open(fpath, 'rb') as fp:
-            data = fp.read()
-        os.remove(fpath)
-        return data
+        return self._camera.get_preview()
 
     def capture(self, path):
         shoot_raw = self.config['shoot_raw'].get(bool)
@@ -157,11 +146,11 @@ class GPhoto2CameraDevice(DeviceDriver):
         extension = 'cr2' if shoot_raw else 'jpg'
         local_path = "{0}.{1}".format(path, extension)
 
-        self._camera.capture_image(local_path)
+        imgdata = self._camera.capture()
 
         # Set EXIF orientation
         self.logger.debug("Setting EXIF orientation on captured image")
-        img = JPEGImage(local_path)
+        img = JPEGImage(blob=imgdata)
         upside_down = self.config["upside_down"].get(bool)
         if self.target_page == 'odd':
             img.exif_orientation = 8 if upside_down else 6  # -90°
@@ -170,6 +159,7 @@ class GPhoto2CameraDevice(DeviceDriver):
         img.save(local_path)
 
     def update_configuration(self, updated):
+        # TODO: Implement
         pass
 
     def _acquire_focus(self):
