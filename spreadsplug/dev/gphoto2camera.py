ffb867a368bd36f8cd534b17a1e010a805cb99f4
Johannes Baiter <johannes.baiter@gmail.com>
2014-11-24 23:14:30 +0100

    gphoto2driver: Remove all configuration options
    The decision to do this is based on the fact that libgphoto2 supports an
    extremely wide range of cameras, the configuration possibilities for
    which vary drastically. This is especially bad between vendors, where
    one and the same functionality can sometimes have up to three or four
    different names and possible values. Since the gphoto2-cffi bindings
    allow changes to the camera configuration through the camera's own UI
    during runtime, it was decided to leave it to the user to manually
    adjust the camera settings himself. This simplifies the spreads
    interfaces significantly, cuts down on moving parts inside the
    application where things can go wrong and keeps ugly workarounds and
    potentially unstable dependencies on libgphoto2 internals out.

--- spreadsplug/dev/gphoto2camera.py
+++ spreadsplug/dev/gphoto2camera.py
@@ -3,7 +3,6 @@ import logging
 
 from jpegtran import JPEGImage
 
-from spreads.config import OptionTemplate
 from spreads.plugin import DeviceDriver, DeviceFeatures
 
 import gphoto2
@@ -19,27 +18,23 @@ class GPhoto2CameraDevice(DeviceDriver):
 
     @classmethod
     def configuration_template(cls):
-        # TODO: The basic problem that prevents us from supporting more
-        # configuration values is that in gphoto2, the available options
-        # are only known when the device is already initialized, which
-        # is not possible since this is ia @classmethod.
-        # We also cannot have the user specify a specific gphoto2 driver,
-        # since at the time this method is called, not even the configuration
-        # is loaded.
-        # The best way to proceed is probably to determine a superset of
-        # configuration values that are of interest to spreads users and
-        # map from those to the driver's specific options.
-        # At first we will likely only support ptp2
-        conf = super(GPhoto2CameraDevice, cls).configuration_template()
-        conf.update(
-            {'iso': OptionTemplate(u"Auto", "The ISO value"),
-             'shutter_speed': OptionTemplate(
-                 u"1/25", "The shutter speed as a fraction"),
-             'aperture': OptionTemplate(
-                 u"5.6", "The shutter speed as an f-stop"),
-             'shoot_raw': OptionTemplate(False, "Shoot in RAW format (DNG)")
-             })
-        return conf
+        # TODO: The basic problem that prevents us from supporting any
+        # configuration values is that in gphoto2, the available options are
+        # only known when the device is already initialized, which is not
+        # possible since this is ia @classmethod.  We also cannot have the user
+        # specify a specific gphoto2 driver, since at the time this method is
+        # called, not even the configuration is loaded.  The best way to
+        # proceed is probably to determine a superset of configuration values
+        # that are of interest to spreads users and map from those to the
+        # driver's specific options.  At first we will likely only support ptp2
+        # TODO: Another significant problem is that even for cameras that
+        # are supported by the ptp2 driver, the names and possible values
+        # of the configuration widgets can differ drastically. A possible
+        # solution for this would be to create subclasses for various,
+        # known-to-work camera models that expose their own configuration
+        # templates and implement the configuration-related methods
+        # themselves.
+        return super(GPhoto2CameraDevice, cls).configuration_template()
 
     @classmethod
     def yield_devices(cls, config):
@@ -90,44 +85,10 @@ class GPhoto2CameraDevice(DeviceDriver):
         # Map this in the spreads config keyed by the camera serial number.
         # We can't use the approach taken by CHDKCameraDevice, because most
         # PTP cameras don't support writing files via PTP.
-        self.config['target_page'][self._serialnumber].set(target_page)
+        self.config['target_page'][self._serial_number].set(target_page)
 
     def prepare_capture(self):
-        iso = str(self.config['iso'].get())
-        shoot_raw = self.config['shoot_raw'].get(bool)
-        shutter_speed = str(self.config["shutter_speed"].get())
-        aperture = str(self.config['aperture'].get())
-
-        cfg = self._camera.config
-        mode = cfg['capturesettings']['autoexposuremode'].value
-
-        self.logger.debug("Camera mode: {}".format(mode))
-        self.logger.debug(
-            "Setting iso={}, shutter_speed={}, aperture={}, raw={}"
-            .format(iso, shutter_speed, aperture, shoot_raw))
-
-        cfg['imgsettings']['iso'].set(iso)
-
-        if shoot_raw:
-            format = 'RAW'
-        else:
-            format = 'Large Fine JPEG'
-        cfg['imgsettings']['imageformat'].set(format)
-        cfg['imgsettings']['imageformatsd'].set(format)
-
-        if cfg['capturesettings']['shutterspeed']:
-            cfg['capturesettings']['shutterspeed'].set(shutter_speed)
-        else:
-            self.logger.debug(
-                "Skipping shutter_speed config due to camera mode ({})"
-                .format(mode))
-
-        if cfg['capturesettings']['aperture']:
-            cfg['capturesettings']['aperture'].set(aperture)
-        else:
-            self.logger.debug(
-                "Skipping aperture config due to camera mode ({})"
-                .format(mode))
+        pass
 
     def finish_capture(self):
         pass
@@ -136,16 +97,6 @@ class GPhoto2CameraDevice(DeviceDriver):
         return self._camera.get_preview()
 
     def capture(self, path):
-        shoot_raw = self.config['shoot_raw'].get(bool)
-
-        # TODO: support choosing jpg size
-        # TODO: support choosing sraw vs raw
-        # TODO: support capturing raw + jpeg
-        # TODO: choose raw extension based on camera vendor
-
-        extension = 'cr2' if shoot_raw else 'jpg'
-        local_path = "{0}.{1}".format(path, extension)
-
         imgdata = self._camera.capture()
 
         # Set EXIF orientation
@@ -159,7 +110,6 @@ class GPhoto2CameraDevice(DeviceDriver):
         img.save(local_path)
 
     def update_configuration(self, updated):
-        # TODO: Implement
         pass
 
     def _acquire_focus(self):
